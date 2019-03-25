e7762d2cfac420697ca845814eee9f1c7eb01977
Gary Servin <garyservin@gmail.com>
2019-03-25 16:56:43 -0400

    Add bare metal image creation to CI (#11)
    * Add option to create bare metal images on to create_image.py
    
    * Simplify rosidstro path and avoid using ansible.cfg
    
    * Add configuration for ansible
    
    * Use sudo for docker only
    
    * Make naming of github token constant
    
    * Handle building of bare metal with publish
    
    * Use link from rosdistro to get correct base image
    
    * Use locus_tools from source and build it before we use
    
    * Remove building from source and address PR comments
    
    * Use boto3 to download the base image
    
    * Move pip installs from Docker to setup.cfg

--- tailor_image/create_image.py
+++ tailor_image/create_image.py
@@ -1,33 +1,70 @@
 #!/usr/bin/python3
 import base64
 import os
+import pathlib
 import sys
 
 import argparse
 import boto3
 import click
 import docker
+import yaml
+
+from catkin.find_in_workspaces import find_in_workspaces
+from . import run_command
 
 
-def create_image(name: str, build_type: str, package: bool, provision_file: str, distribution: str, apt_repo: str,
-                 release_track: str, release_label: str, flavour: str, organization: str, docker_registry: str,
-                 publish: bool = False):
+def create_image(name: str, distribution: str, apt_repo: str, release_track: str, release_label: str, flavour: str,
+                 organization: str, docker_registry: str, rosdistro_path: pathlib.Path, publish: bool = False):
+    """Create different type of images based on recipes
+    :param name: Name for the image
+    :param distribution: Ubuntu distribution to build the image against
+    :param apt_repo: APT repository to get debian packages from
+    :param release_track: The main release track to use for naming, packages, etc
+    :param release_label: Contains the release_track + the label for the most current version
+    :param flavour: Bundle flavour to install on the images
+    :param organization: Name of the organization
+    :param docker_registry: URL for the docker registry to use to push images from/to
+    :param rosdistro_path: Path for the rosdistro configuration files
+    :param publish: Whether to publish the images
+    """
+
+    # Read configuration files
+    recipe = yaml.safe_load((rosdistro_path / 'config/images.yaml').open())['images']
+    build_type = recipe[name]['build_type']
+    package = recipe[name]['package']
+    provision_file = recipe[name]['provision_file']
+
     if build_type == 'docker':
         create_docker_image(name=name, dockerfile=provision_file, distribution=distribution, apt_repo=apt_repo,
                             release_track=release_track, flavour=flavour, release_label=release_label,
                             organization=organization, publish=publish, docker_registry=docker_registry)
 
-    elif build_type == 'bare_metal':
-        create_bare_metal_image(provision_file)
-
-
-def create_bare_metal_image(provision_file: str):
-    # TODO(gservin): Add steps to create bare metal on CI [RST-1668]
-    click.echo(f'Building bare metal image with: {provision_file}', err=True)
+    # Building takes around 1,5 hours, build only if publish is set to true
+    # TODO(gservin): Only build bare_metal if we're on xenial for now, add a better check
+    elif build_type == 'bare_metal' and publish and distribution == 'xenial':
+        # Get information about base image
+        base_image = recipe[name]['base_image']
+        base_image_checksum = recipe[name]['base_image_checksum']
+        create_bare_metal_image(image_name=name, package=package, provision_file=provision_file, s3_bucket=apt_repo,
+                                release_track=release_track, base_image=base_image,
+                                base_image_checksum=base_image_checksum)
 
 
 def create_docker_image(name: str, dockerfile: str, distribution: str, apt_repo: str, release_track: str, flavour: str,
                         release_label: str, organization: str, docker_registry: str, publish: bool):
+    """Create docker images
+    :param name: Name for the image
+    :param dockerfile: Dockerfile to use to build the image
+    :param distribution: Ubuntu distribution to build the image against
+    :param apt_repo: APT repository to get debian packages from
+    :param release_track: The main release track to use for naming, packages, etc
+    :param release_label: Contains the release_track + the label for the most current version
+    :param flavour: Bundle flavour to install on the images
+    :param organization: Name of the organization
+    :param docker_registry: URL for the docker registry to use to push images from/to
+    :param publish: Whether to publish the images
+    """
 
     click.echo(f'Building docker image with: {dockerfile}')
     docker_client = docker.from_env()
@@ -97,13 +134,63 @@ def process_docker_api_line(line):
         click.echo(line["status"], err=True)
 
 
+def create_bare_metal_image(image_name: str, package: str, provision_file: str, s3_bucket: str, release_track: str,
+                            base_image: str, base_image_checksum: str):
+    """Create bare metal image using packer and provisioned via ansible
+    :param name: Name for the image
+    :param package: Package containing the configuration files
+    :param provision_file: Name of the ansible playbook to use to provision the image
+    :param s3_bucket: S3 bucket to push the image to
+    :param base_image: Image name on the s3_bucket to use as base
+    :param base_image_checksum: Checksum of the base image
+    """
+
+    click.echo(f'Building bare metal image with: {provision_file}', err=True)
+
+    # Get path to the different files needed
+    provision_file_path = find_package(package, 'playbooks/' + provision_file)
+    template_path = find_package(package, 'image_recipes/bare_metal/bare_metal.json')
+    cloud_cfg_path = find_package(package, 'image_recipes/bare_metal/cloud.cfg')
+    cloud_img_path = '/tmp/cloud.cfg'
+
+    os.environ['ANSIBLE_CONFIG'] = find_package(package, 'ansible.cfg')
+
+    # Get base image
+    s3_object = boto3.resource('s3')
+    base_image_local_path = '/tmp/' + base_image
+    base_image_key = release_track + '/images/' + base_image
+    s3_object.Bucket(s3_bucket).download_file(base_image_key, base_image_local_path)
+
+    # Generate cloud.img
+    run_command(['cloud-localds', cloud_img_path, cloud_cfg_path])
+
+    command = ['packer', 'build',
+               '-var', f'vm_name={image_name}',
+               '-var', f'playbook_file={provision_file_path}',
+               '-var', f's3_bucket={s3_bucket}',
+               '-var', f'cloud_image={cloud_img_path}',
+               '-var', f'iso_url={base_image_local_path}',
+               '-var', f'iso_checksum={base_image_checksum}',
+               '-timestamp-ui',
+               template_path]
+
+    run_command(command)
+
+
+def find_package(package_name: str, filename: str):
+    package_path = find_in_workspaces(
+        project=package_name,
+        path=filename,
+        first_match_only=True,
+    )[0]
+
+    return package_path
+
+
 def main():
     parser = argparse.ArgumentParser(description=create_image.__doc__)
     parser.add_argument('--name', type=str, required=True)
-    parser.add_argument('--build-type', type=str, required=True)
-    parser.add_argument('--package', type=str, required=True)
-    parser.add_argument('--provision-file', type=str, required=True)
-    parser.add_argument('--distribution', type=str)
+    parser.add_argument('--distribution', type=str, required=True)
     parser.add_argument('--apt-repo', type=str)
     parser.add_argument('--release-track', type=str)
     parser.add_argument('--release-label', type=str)
@@ -111,6 +198,7 @@ def main():
     parser.add_argument('--organization', type=str)
     parser.add_argument('--publish', action='store_true')
     parser.add_argument('--docker-registry', type=str)
+    parser.add_argument('--rosdistro-path', type=pathlib.Path)
 
     args = parser.parse_args()
 
