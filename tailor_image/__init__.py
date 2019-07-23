145d565f4048536f808484f7662f9fa78d2a2b73
Gary Servin <gservin@locusrobotics.com>
2019-07-23 16:53:58 -0400

    Remove catkin.find_workspaces (#27)
    * Remove catkin.find_workspaces
    
    * Add back rosdistro-path argument
    
    * Don't modify systems's environment variables

--- tailor_image/__init__.py
+++ tailor_image/__init__.py
@@ -1,9 +1,24 @@
 __version__ = '0.0.0'
 
+import json
+import os
 import sys
 import subprocess
+
+
+def find_package(package: str, path: str, env):
+    path = run_command(['catkin_find', package, path, '--first-only'],
+                       stdout=subprocess.PIPE,
+                       env=env).stdout.decode().replace('\n', '')
+    return path
 
 
 def run_command(cmd, *args, **kwargs):
     print(' '.join(cmd), file=sys.stderr)
     return subprocess.run(cmd, check=True, *args, **kwargs)
+
+
+def source_file(path):
+    dump = '/usr/bin/python -c "import os, json; print json.dumps(dict(os.environ))"'
+    pipe = subprocess.Popen(['/bin/bash', '-c', f'source {path} && {dump}'], stdout=subprocess.PIPE)
+    return json.loads(pipe.stdout.read())
--- tailor_image/create_image.py
+++ tailor_image/create_image.py
@@ -13,8 +13,7 @@ import botocore
 import click
 import yaml
 
-from catkin.find_in_workspaces import find_in_workspaces
-from . import run_command
+from . import find_package, run_command, source_file
 
 
 def create_image(name: str, distribution: str, apt_repo: str, release_track: str, release_label: str, flavour: str,
@@ -35,10 +34,12 @@ def create_image(name: str, distribution: str, apt_repo: str, release_track: str
     # Read configuration files
     common_config = yaml.safe_load((rosdistro_path / 'config/recipes.yaml').open())['common']
     recipe = yaml.safe_load((rosdistro_path / 'config/images.yaml').open())['images']
+    distro = recipe[name]['distro']
     build_type = recipe[name]['build_type']
     package = recipe[name]['package']
     provision_file = recipe[name]['provision_file']
-    template_path = find_package(package, f'image_recipes/{name}/{name}.json')
+    env = source_file(f'{os.environ["BUNDLE_ROOT"]}/{distro}/setup.bash')
+    template_path = find_package(package, f'image_recipes/{name}/{name}.json', env)
     today = datetime.date.today().strftime('%Y%m%d')
     extra_vars = []  # type: List[Any]
 
@@ -70,15 +71,6 @@ def create_image(name: str, distribution: str, apt_repo: str, release_track: str
         base_image_key = release_track + '/images/' + base_image
         boto3.resource('s3').Bucket(apt_repo).download_file(base_image_key, base_image_local_path)
 
-        # Generate image name
-        image_name = f'{organization}_{name}_{distribution}_{release_label}_{today}'
-
-        extra_vars = [
-            '-var', f'vm_name={image_name}',
-            '-var', f's3_bucket={apt_repo}',
-            '-var', f'iso_image={base_image_local_path}'
-        ]
-
         # Enable nbd kernel module, necesary for qemu's packer chroot builder
         run_command(['modprobe', 'nbd'])
 
@@ -92,22 +84,32 @@ def create_image(name: str, distribution: str, apt_repo: str, release_track: str
         # Resize partition inside qcow image
         run_command(['virt-resize', '--expand', '/dev/sda1', base_image_local_path, tmp_image])
         run_command(['mv', tmp_image, base_image_local_path])
+
+        # Generate image name
+        image_name = f'{organization}_{name}_{distribution}_{release_label}_{today}'
+
+        extra_vars = [
+            '-var', f'vm_name={image_name}',
+            '-var', f's3_bucket={apt_repo}',
+            '-var', f'iso_image={base_image_local_path}'
+        ]
+
     else:
         return 0
 
     click.echo(f'Building {build_type} image with: {provision_file}', err=True)
 
     # Get path to the different files needed
-    provision_file_path = find_package(package, 'playbooks/' + provision_file)
+    provision_file_path = find_package(package, 'playbooks/' + provision_file, env)
 
-    os.environ['ANSIBLE_CONFIG'] = find_package(package, 'ansible.cfg')
+    env['ANSIBLE_CONFIG'] = find_package(package, 'ansible.cfg', env)
 
     command = ['packer', 'build',
                '-var', f'playbook_file={provision_file_path}',
                '-var', f'bundle_track={release_track}',
                '-var', f'bundle_version={release_label}'] + extra_vars + ['-timestamp-ui', template_path]
 
-    run_command(command)
+    run_command(command, env=env)
 
     # TODO(gservin): If we build more that one bare metal image at the same time, we can have a race condition here
     if build_type == 'bare_metal' and publish and distribution == 'xenial':
@@ -160,16 +162,6 @@ def update_image_index(release_track, apt_repo, common_config, image_name):
                                        },
                                        'CallerReference':  datetime.date.today().strftime('%Y%m%d%H%M%S')
                                    })
-
-
-def find_package(package_name: str, filename: str):
-    package_path = find_in_workspaces(
-        project=package_name,
-        path=filename,
-        first_match_only=True,
-    )[0]
-
-    return package_path
 
 
 def main():
