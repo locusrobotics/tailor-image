0a7251802500ddfb3c21b0b81c64702fcdc3c76c
Gary Servin <gservin@locusrobotics.com>
2021-01-11 16:38:54 -0300

    Use same timestamp for multiple images

--- tailor_image/__init__.py
+++ tailor_image/__init__.py
@@ -81,3 +81,20 @@ def invalidate_file_cloudfront(distribution_id, key):
                                    },
                                    'CallerReference':  datetime.now().strftime('%Y%m%d%H%M%S')
                                })
+
+
+def merge_dicts(dict_a, dict_b, path=None):
+    "Merges dictionary b into dictionary a"
+    if path is None:
+        path = []
+    for key in dict_b:
+        if key in dict_a:
+            if isinstance(dict_a[key], dict) and isinstance(dict_b[key], dict):
+                merge_dicts(dict_a[key], dict_b[key], path + [str(key)])
+            elif dict_a[key] == dict_b[key]:
+                pass  # same leaf value
+            else:
+                raise Exception('Conflict at %s' % '.'.join(path + [str(key)]))
+        else:
+            dict_a[key] = dict_b[key]
+    return dict_a
